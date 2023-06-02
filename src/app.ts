4ef0bbcfada881f27dec4b2e513103a005e06584
Tahjib Al Sakin <sakin@mac.local>
2023-06-03 02:41:28 +0600

    setup project

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
 
-const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: any) => {
+  res.send('Server is running')
+})
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
-
-export default app;
+export default app
